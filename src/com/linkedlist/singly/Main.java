16a1b8ba1f2a1db1a6bc7eb3e48c58499ab5e554
unknown <harrygenius099@gmail.com>
2016-06-11 12:58:19 +0530

    LinkedList added

---
+++ src/com/linkedlist/singly/Main.java
@@ -0,0 +1,5 @@
+package com.linkedlist.singly;
+
+public class Main {
+
+}
---
+++ src/com/linkedlist/singly/SinglyLinkedList.java
@@ -0,0 +1,22 @@
+package com.linkedlist.singly;
+
+public class SinglyLinkedList<T> {
+
+	private Node<T> root;
+
+	public Node<T> getRoot() {
+		return root;
+	}
+
+	public void setRoot(Node<T> root) {
+		this.root = root;
+	}
+
+	public SinglyLinkedList() {
+		this.root = null;
+	}
+	
+	
+	
+
+}
